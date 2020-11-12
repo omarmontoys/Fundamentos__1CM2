7a1c0521fd251d3f070c38d5da5712a577c5c76f
omarmontoys <72107693+omarmontoys@users.noreply.github.com>
2020-11-12 13:27:29 -0600

    actualizacion de codigo

--- Fundamentos_1CM2/Tablas_de_multiplicar.c
+++ Fundamentos_1CM2/Tablas_de_multiplicar.c
@@ -3,15 +3,17 @@
 
 int main(int argc, char** argv)
 {
-    int x = 1;
-    int t = 1;
+    int x ;
+    printf("ingrese con que tabla quiere iniciar\n");
+    scanf("%d", &x);
+    int t= 1;
     int r;
     printf("ingrese hasta que numero desea multiplicar\n");
     scanf("%d", &r);
     int l;
     printf("ingrese hasta que tabla quiere multiplicar\n");
     scanf ("%d", &l);
-    printf("Tabla del %d \n",t);
+
     while (x<=l ){
             printf("Tabla del %d \n",x);
             while(t <= r){
